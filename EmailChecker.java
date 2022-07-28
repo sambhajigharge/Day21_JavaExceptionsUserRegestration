f51e953bb286c1b03439b2fc7819b3c73b9248ad
sushant <sgharge299@gmail.com>
2022-07-28 14:40:13 +0530

    [sushant]started the program with welcome message

---
+++ EmailChecker.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class EmailChecker {
+    public static void main(String[] args) {
+        System.out.println("welcome to the program of exception handling of user registration");
+    }
+}
