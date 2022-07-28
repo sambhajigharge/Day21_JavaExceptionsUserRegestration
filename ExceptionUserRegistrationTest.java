d5d239dfa5709c39e86fbafa5deaa974fe3a65ca
sushant <sgharge299@gmail.com>
2022-07-28 15:18:51 +0530

    [sushant]checking for mobile number

--- ExceptionUserRegistrationTest.java
+++ ExceptionUserRegistrationTest.java
@@ -27,9 +27,33 @@ public class ExceptionUserRegistrationTest {
     }
 
     @Test
+    public void givenMobileNumber_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.mobileNumber("91 9989774456");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenMobileNumber_IsNotProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.mobileNumber("919989774456");
+        Assert.assertEquals(false, actualResult);
+    }
+
+    @Test
     public void givenLastName_IsProper_ReturnFalse() {
         boolean actualResult = exceptionUserRegistration.lastName("VeLiseTTi");
         Assert.assertEquals(false, actualResult);
     }
-    
+
+    @Test
+    public void givenEmailId_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.emailId("ramakrishna96.velisetti@gmail.com");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenEmailId_IsNotProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.emailId("ramakrishna96.velisettigmail.com");
+        Assert.assertEquals(false, actualResult);
+    }
+
 }
